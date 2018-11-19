c3dde3a856f8e8e92cb232470ec5a7d48a463b40
jbarbadillo <javier.barbadillo@gmail.com>
2018-11-19 19:32:03 +0100

    Add registration model

---
+++ app/src/main/java/com/javirock/cleanarchevents/usecases/RegistrationModel.java
@@ -0,0 +1,10 @@
+package com.javirock.cleanarchevents.usecases;
+
+import com.javirock.cleanarchevents.entities.RegistrationData;
+
+public class RegistrationModel {
+    public void requestRegistration(RegistrationData data){
+        // api.request(data);
+    }
+}
+
