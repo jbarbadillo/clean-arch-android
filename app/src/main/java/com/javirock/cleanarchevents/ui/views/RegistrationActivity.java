f2af14ef092748eb7d3e6cc7069d6fb68543f097
jbarbadillo <javier.barbadillo@gmail.com>
2018-11-19 19:52:11 +0100

    Add registration activity

---
+++ app/src/main/java/com/javirock/cleanarchevents/ui/views/RegistrationActivity.java
@@ -0,0 +1,21 @@
+package com.javirock.cleanarchevents.ui.views;
+
+import android.support.v7.app.AppCompatActivity;
+
+
+public class RegistrationActivity extends AppCompatActivity implements RegistrationView {
+    @Override
+    public void showProgress() {
+
+    }
+
+    @Override
+    public void hideProgress() {
+
+    }
+
+    @Override
+    public void showError(String error) {
+
+    }
+}
