ba2de80cf48fea63cc5e4e80830e4a655d2bfcab
jbarbadillo <javier.barbadillo@gmail.com>
2018-11-20 19:41:20 +0100

    Refactor presenter

--- app/src/main/java/com/javirock/cleanarchevents/presentationlayer/presenters/RegistrationPresenter.java
+++ app/src/main/java/com/javirock/cleanarchevents/presentationlayer/presenters/RegistrationPresenter.java
@@ -1,8 +1,12 @@
 package com.javirock.cleanarchevents.presentationlayer.presenters;
 
-/**
- * Created by javier on 20/11/2018.
- */
 
 public interface RegistrationPresenter {
+    void onRegisterClicked(String userName, String password, String email);
+    interface View{
+        void showProgress();
+        void hideProgress();
+        void showError(String error);
+    }
+
 }
--- app/src/main/java/com/javirock/cleanarchevents/presentationlayer/presenters/impl/RegistrationPresenterImpl.java
+++ app/src/main/java/com/javirock/cleanarchevents/presentationlayer/presenters/impl/RegistrationPresenterImpl.java
@@ -1,5 +1,33 @@
 package com.javirock.cleanarchevents.presentationlayer.presenters.impl;
 
 
-public class RegistrationPresenterImpl {
+import com.javirock.cleanarchevents.businesslayer.interactors.RegistrationInteractor;
+import com.javirock.cleanarchevents.businesslayer.repositories.RegistrationRepository;
+import com.javirock.cleanarchevents.presentationlayer.presenters.RegistrationPresenter;
+
+public class RegistrationPresenterImpl implements RegistrationPresenter, RegistrationInteractor.CallBack{
+
+    RegistrationPresenter.View view;
+    RegistrationRepository registrationRepository;
+    RegistrationInteractor registrationInteractor;
+
+    RegistrationPresenterImpl(View view, RegistrationRepository registrationRepository){
+        this.view = view;
+        this.registrationRepository = registrationRepository;
+        this.registrationInteractor = new RegistrationInteractor(this, registrationRepository);
+    }
+    @Override
+    public void onRegisterClicked(String userName, String password, String email) {
+
+    }
+
+    @Override
+    public void onRegistration(String user_id) {
+
+    }
+
+    @Override
+    public void onRegistrationFailed(String error) {
+
+    }
 }
