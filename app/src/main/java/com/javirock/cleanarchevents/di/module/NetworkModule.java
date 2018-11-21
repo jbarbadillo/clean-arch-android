4723466970fe2fda25ce10d425800460bc7c6c50
jbarbadillo <javier.barbadillo@gmail.com>
2018-11-21 21:27:58 +0100

    Add network component

--- app/src/main/java/com/javirock/cleanarchevents/di/module/NetworkModule.java
+++ app/src/main/java/com/javirock/cleanarchevents/di/module/NetworkModule.java
@@ -1,5 +1,17 @@
 package com.javirock.cleanarchevents.di.module;
 
+import android.content.Context;
 
+import dagger.Module;
+
+@Module
 public class NetworkModule {
+    private String baseUrl;
+    private Context context;
+
+    public NetworkModule(Context context, String baseUrl){
+        this.baseUrl = baseUrl;
+        this.context = context;
+    }
 }
+
