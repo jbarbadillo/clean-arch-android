95aa443cbc795a2fbb95114b681ba8c318bad808
jbarbadillo <javier.barbadillo@gmail.com>
2018-11-21 21:32:51 +0100

    Add network component

--- app/src/main/java/com/javirock/cleanarchevents/di/module/NetworkModule.java
+++ app/src/main/java/com/javirock/cleanarchevents/di/module/NetworkModule.java
@@ -2,7 +2,14 @@ package com.javirock.cleanarchevents.di.module;
 
 import android.content.Context;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class NetworkModule {
@@ -13,5 +20,23 @@ public class NetworkModule {
         this.baseUrl = baseUrl;
         this.context = context;
     }
+
+    @Singleton
+    @Provides
+    Retrofit providesRetrofit(OkHttpClient client,
+                              GsonConverterFactory gsonConverterFactory,
+                              RxJavaCallAdapterFactory rxJavaCallAdapterFactory){
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(gsonConverterFactory)
+                .addCallAdapterFactory(rxJavaCallAdapterFactory)
+                .client(client)
+                .build();
+
+
+    }
+    @Singleton
+    @Provides
+    Context providesContext(){return context;}
 }
 
