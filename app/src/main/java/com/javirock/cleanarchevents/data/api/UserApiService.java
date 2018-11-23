31311924c82cfaca0511bbb87e6807b2c346b453
jbarbadillo <javier.barbadillo@gmail.com>
2018-11-23 17:38:04 +0100

    Create observable get user

--- app/src/main/java/com/javirock/cleanarchevents/data/api/UserApiService.java
+++ app/src/main/java/com/javirock/cleanarchevents/data/api/UserApiService.java
@@ -1,6 +1,15 @@
 package com.javirock.cleanarchevents.data.api;
 
 
+import com.javirock.cleanarchevents.businesslayer.models.UserModel;
 
-public class UserApiService {
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+
+public interface UserApiService {
+
+    //With Rx Java
+    @GET("/user")
+    Observable<UserModel> getUser();
+
 }
