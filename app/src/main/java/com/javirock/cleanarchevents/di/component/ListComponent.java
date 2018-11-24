bd3086d20c1987a957fa87fb7d0b9751bb8a85a4
jbarbadillo <javier.barbadillo@gmail.com>
2018-11-24 13:41:48 +0100

    Implement list component

--- app/src/main/java/com/javirock/cleanarchevents/di/component/ListComponent.java
+++ app/src/main/java/com/javirock/cleanarchevents/di/component/ListComponent.java
@@ -1,5 +1,12 @@
 package com.javirock.cleanarchevents.di.component;
 
+import com.javirock.cleanarchevents.di.module.UserModule;
+import com.javirock.cleanarchevents.di.scope.PerActivity;
 
+import dagger.Component;
+
+@PerActivity
+@Component(modules= UserModule.class, dependencies = ApplicationComponent.class)
 public interface ListComponent {
+    void inject(MainActivity activity);
 }
