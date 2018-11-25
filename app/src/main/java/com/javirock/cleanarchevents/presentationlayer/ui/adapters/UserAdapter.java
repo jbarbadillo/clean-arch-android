3781eea24385d549cfb1d038fc0b63ef5492f74a
jbarbadillo <javier.barbadillo@gmail.com>
2018-11-25 12:50:28 +0100

    Implement list adapter

--- app/src/main/java/com/javirock/cleanarchevents/presentationlayer/ui/adapters/UserAdapter.java
+++ app/src/main/java/com/javirock/cleanarchevents/presentationlayer/ui/adapters/UserAdapter.java
@@ -1,29 +1,57 @@
 package com.javirock.cleanarchevents.presentationlayer.ui.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
+import com.javirock.cleanarchevents.R;
+import com.javirock.cleanarchevents.businesslayer.models.UserModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class UserAdapter extends BaseAdapter {
+    LayoutInflater layoutInflater;
+    Context context;
+    private List<UserModel> userModelList = new ArrayList<>();
+
+    public UserAdapter(Context context){
+        this.layoutInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+    public void addItems(List<UserModel> userModelList){
+        userModelList.addAll(userModelList);
+        notifyDataSetChanged();
+    }
     @Override
     public int getCount() {
-        return 0;
+        return userModelList.size();
     }
-
     @Override
-    public Object getItem(int i) {
-        return null;
+    public Object getItem(int position) {
+        return userModelList.get(position);
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+        View view = LayoutInflater.from(context).inflate(R.layout.user_list, null);
+        ButterKnife.bind(this, view);
+        titleView.setText(userModelList.get(i).getLogin());
+        return view;
     }
+
+    @BindView(R.id.title)
+    TextView titleView;
 }
